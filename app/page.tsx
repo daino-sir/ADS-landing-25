5c58030c2d0dcc61c70952fdad07557ceeb9eb62
Daino Otieno <72624000+daino-sir@users.noreply.github.com>
2025-08-06 19:58:54 +0300

    rm speakers section (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             <Hero />
             <About />
             <HallOfFame />
-            <Speakers />
+            {/* <Speakers /> */}
             <Schedule />
             <Pricing />
             <Footer />
